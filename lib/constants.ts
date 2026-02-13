88aab754f1b1ba6434b21b0b34c9e93b913bf7f0
brandox02 <brandox02.dev@gmail.com>
2026-02-13 17:03:08 -0400

    Update launch date in constants to February 28, 2026.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 // Fecha de lanzamiento: Viernes 14 de Febrero, 2026 • 3PM EST
-export const LAUNCH_DATE = new Date('February 14, 2026 15:00:00 EST').getTime();
+export const LAUNCH_DATE = new Date('February 28, 2026 15:00:00 EST').getTime();
 
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://turnera.com';
